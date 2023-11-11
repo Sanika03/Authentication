711478126e6bf2686b7919a1ea554ba8eea032e7
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-11 14:05:25 +0530

    Add custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,51 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitter-blue": "#1D9BF0",
+        "twitter-blue-hover": "#1871CA",
+        "neutral-50": "#F9F9F9",
+        "neutral-200": "#E4E4E4",
+        "neutral-400": "#A2A2A2",
+        "neutral-500": "#737373",
+        "neutral-600": "#525252",
+        "neutral-700": "#404040",
+        "neutral-800": "#262626",
+        "neutral-1000": "#000000",
+        "button-stroke": "#546A7A",
+        "searchbar-fill": "#212327",
+        "secondary": "rgba(255, 255, 255, 0.6)",
+      },
+      fontFamily: {
+        "inter": ['Inter', 'sans-serif']
+      },
+      fontSize: {
+        "15": "15px"
+      },
+      lineHeight: {
+        "normal": "normal"
+      },
+      borderRadius: {
+        "border-65": "65px",
+        "border-9999": "9999px",
+        "border-30": "30px",
+        "border-32": "32px"
+      },
+      boxShadow: {
+        "box-shadow": "0px 8px 16px 0px rgba(0, 0, 0, 0.25)"
+      },
+      backdropBlur: {
+        "23": "blur(23.668209075927734px)"
+      },
+      width: {
+        "390": "398px",
+        "868" : "868px"
+      },
+      gap: {
+        "1.25": "5px"
+      }
+    },
   },
   plugins: [],
 }
