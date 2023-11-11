1be96cb37598d142fd467b7f224251413732e2b7
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-11 14:08:32 +0530

    Add routes for home, login and remove api handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute><Home/></ProtectedRoute>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+])
+
 function App() {
-  const API_URL = "https://one00x-react-backend.onrender.com";
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch(API_URL + "/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: "batman@example.com",
-          password: "gothamrocks",
-        }),
-      });
-
-      if (!response.ok) {
-        return alert(`Error: ${response.status - response.statusText}`)
-      }
-
-      console.log(await response.json())
-      //navigate to feed
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  };
-  
-  handleLogin();  
-  
   return (
     <>
-      <h1>Hello World</h1>
+      <RouterProvider router={router}/>
     </>
   )
 }
