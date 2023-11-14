3643d8455a3db56bf96be3a53d306c1af629e088
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-14 10:44:03 +0530

    Refactor import statements to use default imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { ProtectedRoute } from "./components/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
