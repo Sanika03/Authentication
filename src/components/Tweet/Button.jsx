4bf211732c5a1e963b4580e99816d36d51253808
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-14 21:32:15 +0530

    Create Button component with dynamic styling

---
+++ src/components/Tweet/Button.jsx
@@ -0,0 +1,23 @@
+const Button = ({ variant, size, text }) => {
+    const baseStyles = "py-2 px-6 rounded-border-65 shadow-box-shadow backdrop-blur-23 text-center font-bold leading-normal w-full";
+
+    const sizeStyles = {
+        sm: "text-sm",
+        md: "text-base",
+        lg: "text-lg"
+    };
+
+    const variantStyles = {
+        outline: "border border-solid border-button-stroke text-twitter-blue"
+    };
+
+    const classes = `${baseStyles} ${sizeStyles[size]} ${variantStyles[variant]}`;
+
+    return (
+            <button className={classes}>
+                {text}
+            </button>
+        )
+}
+
+export default Button;
